9689991b28a8c3f26b05db31ee2cfef31a221cdd
Dima Drogalov <a.drogalov2015@yandex.ru>
2024-08-10 17:20:26 +0300

    Поправляет оформление в блоке отзывы

--- source/js/reviews-slider.js
+++ source/js/reviews-slider.js
@@ -5,5 +5,20 @@ new Swiper('.reviews__container', {
     nextEl: '.reviews-button--right',
     prevEl: '.reviews-button--left'
   },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1366: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
   speed: 500,
 });
